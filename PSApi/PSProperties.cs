04cee8e11172dba47a3f0b2e4ab19d93fdb407fb
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2014-10-13 04:20:19 -0600

    Removed the prop prefix.

--- PSApi/PSProperties.cs
+++ PSApi/PSProperties.cs
@@ -23,120 +23,124 @@ namespace PSFilterLoad.PSApi
         /// The horizontal big nudge distance, 16.16 fixed point value - 'bndH'
         /// </summary>
         /// <remarks>The horizontal distance in pixels to move when using the shift key, default 10 pixels.</remarks>
-        public const uint propBigNudgeH = 0x626e6448U;
+        public const uint BigNudgeH = 0x626e6448U;
         /// <summary>
         /// The vertical big nudge distance, 16.16 fixed point value - 'bndV'
         /// </summary>
         /// <remarks>The vertical distance in pixels to move when using the shift key, default 10 pixels.</remarks>
-        public const uint propBigNudgeV = 0x626e6456U;
+        public const uint BigNudgeV = 0x626e6456U;
         /// <summary>
         /// The caption of the document as an IPTC-NAA record - 'capt'
         /// </summary>
-        public const uint propCaption = 0x63617074U;
+        public const uint Caption = 0x63617074U;
         /// <summary>
         /// Channel Name - 'nmch'
         /// </summary>
-        public const uint propChannelName = 0x6e6d6368U;
+        public const uint ChannelName = 0x6e6d6368U;
         /// <summary>
         /// The copyright status of the document - 'cpyr'
         /// </summary>
-        public const uint propCopyright = 0x63707972U;
+        public const uint Copyright = 0x63707972U;
         /// <summary>
-        /// The new copyright property from 5.0, a get only version of propCopyright - 'cpyR'
+        /// The new copyright property from 5.0, a get only version of Copyright - 'cpyR'
         /// </summary>
-        public const uint propCopyright2 = 0x63707952U;
+        public const uint Copyright2 = 0x63707952U;
         /// <summary>
         /// The document EXIF data - 'EXIF' 
         /// </summary>
-        public const uint propEXIFData = 0x45584946U;
+        public const uint EXIFData = 0x45584946U;
         /// <summary>
         /// The document XMP data - 'xmpd' 
         /// </summary>
-        public const uint propXMPData = 0x786d7064U;
+        public const uint XMPData = 0x786d7064U;
         /// <summary>
         /// Major grid size, 16.16 fixed point value - 'grmj'      
         /// </summary>
-        /// <remarks>Measured in inches unless <see cref="propRulerUnits"/> is pixels, and then in pixels.</remarks>
-        public const uint propGridMajor = 0x67726d6aU;
+        /// <remarks>Measured in inches unless <see cref="RulerUnits"/> is pixels, and then in pixels.</remarks>
+        public const uint GridMajor = 0x67726d6aU;
         /// <summary>
-        /// Minor grid size, the number of subdivisions per <see cref="propGridMajor"/> - 'grmn'
+        /// Minor grid size, the number of subdivisions per <see cref="GridMajor"/> - 'grmn'
         /// </summary>
-        public const uint propGridMinor = 0x67726d6eU;
+        public const uint GridMinor = 0x67726d6eU;
         /// <summary>
         /// Image mode - 'mode'
         /// </summary>
-        public const uint propImageMode = 0x6d6f6465U;
+        public const uint ImageMode = 0x6d6f6465U;
         /// <summary>
         /// Interpolation Mode - 'intp'
         /// </summary>
-        public const uint propInterpolationMethod = 0x696E7470U;
+        public const uint InterpolationMethod = 0x696E7470U;
         /// <summary>
         /// Number of channels - 'nuch'
         /// </summary>
-        public const uint propNumberOfChannels = 0x6e756368U;
+        public const uint NumberOfChannels = 0x6e756368U;
         /// <summary>
         /// The number of paths = 'nupa'
         /// </summary>
-        public const uint propNumberOfPaths = 0x6e757061U;
+        public const uint NumberOfPaths = 0x6e757061U;
         /// <summary>
         /// The name of the path = 'nmpa'
         /// </summary>
-        public const uint propPathName = 0x6e6d7061U;
+        public const uint PathName = 0x6e6d7061U;
         /// <summary>
         /// The index of the work path = 'wkpa'
         /// </summary>
-        public const uint propWorkPathIndex = 0x776b7061U;
+        public const uint WorkPathIndex = 0x776b7061U;
         /// <summary>
         /// The index of the clipping path = 'clpa'
         /// </summary>
-        public const uint propClippingPathIndex = 0x636c7061U;
+        public const uint ClippingPathIndex = 0x636c7061U;
         /// <summary>
         /// The index of the target path = 'tgpa'
         /// </summary>
-        public const uint propTargetPathIndex = 0x74677061U;
+        public const uint TargetPathIndex = 0x74677061U;
         /// <summary>
         /// Ruler Units - 'rulr'
         /// </summary>
-        public const uint propRulerUnits = 0x72756c72U;
+        public const uint RulerUnits = 0x72756c72U;
         /// <summary>
         /// Ruler origin horizontal, 16.16 fixed point value - 'rorH'
         /// </summary>
-        public const uint propRulerOriginH = 0x726f7248U;
+        public const uint RulerOriginH = 0x726f7248U;
         /// <summary>
         /// Ruler origin vertical, 16.16 fixed point value - 'rorV'
         /// </summary>
-        public const uint propRulerOriginV = 0x726f7256U;
+        public const uint RulerOriginV = 0x726f7256U;
         /// <summary>
         /// The host's serial number string - 'sstr' 
         /// </summary>
-        public const uint propSerialString = 0x73737472U;
+        public const uint SerialString = 0x73737472U;
         /// <summary>
         /// The property that indicates if the file has a digital signature or watermark - 'watr'
         /// </summary>
-        public const uint propWatermark = 0x77617472U;
+        public const uint Watermark = 0x77617472U;
         /// <summary>
         /// The URL of the document - 'URL '
         /// </summary>
-        public const uint propURL = 0x55524c20U;
+        public const uint URL = 0x55524c20U;
         /// <summary>
         /// The title of the document - 'titl'
         /// </summary>
-        public const uint propTitle = 0x7469746cU;
+        public const uint Title = 0x7469746cU;
+        /// <summary>
+        /// The title of the document in UTF16 - 'unnm'
+        /// </summary>
+        public const uint UnicodeTitle = 0x756e6e6dU;
         /// <summary>
         /// The watch suspension level - 'wtch'
         /// </summary>
-        public const uint propWatchSuspension = 0x77746368U;
+        public const uint WatchSuspension = 0x77746368U;
         /// <summary>
         /// The width of the current document in pixels - 'docW'  
         /// </summary>
-        public const uint propDocumentWidth = 0x646f6357U;
+        public const uint DocumentWidth = 0x646f6357U;
         /// <summary>
         /// The height of the current document in pixels - 'docH'  
         /// </summary>
-        public const uint propDocumentHeight = 0x646f6348U;
+        public const uint DocumentHeight = 0x646f6348U;
         /// <summary>
         /// Tool tip display - 'tltp'
         /// </summary>
-        public const uint propToolTips = 0x746c7470U;
+        public const uint ToolTips = 0x746c7470U;
     }
 }
