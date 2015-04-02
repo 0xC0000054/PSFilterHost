fc5f5c652d699ef959578336cc814493b740c9a0
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2015-04-01 18:18:23 -0600

    Added OS.cs

---
+++ OS.cs
@@ -0,0 +1,37 @@
+﻿/////////////////////////////////////////////////////////////////////////////////
+//
+// 8bf filter host for .NET
+// http://psfilterhost.codeplex.com/
+//
+// This software is provided under the Microsoft Public License:
+//   Copyright (C) 2012-2015 Nicholas Hayes
+// 
+// See LICENSE.txt for complete licensing and attribution information.
+//
+/////////////////////////////////////////////////////////////////////////////////
+
+using System;
+
+namespace PSFilterHostDll
+{
+    internal static class OS
+    {
+        private static bool checkedIsWindows7OrLater;
+        private static bool isWindows7OrLater;
+
+        public static bool IsWindows7OrLater
+        {
+            get
+            {
+                if (!checkedIsWindows7OrLater)
+                {
+                    OperatingSystem os = Environment.OSVersion;
+                    isWindows7OrLater = (os.Platform == PlatformID.Win32NT && ((os.Version.Major == 6 && os.Version.Minor >= 1) || os.Version.Major > 6));
+                    checkedIsWindows7OrLater = true;
+                }
+
+                return isWindows7OrLater;
+            }
+        }
+    }
+}
