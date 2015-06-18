ff4ccbe21770e406ca97beaa27e6ffb689418a2e
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2015-06-18 07:36:02 -0600

    Changed the Extract method to use TIFF for the XMP data, added the TIFFReader class.

--- PSApi/ImageMetaData.cs
+++ PSApi/ImageMetaData.cs
@@ -37,7 +37,7 @@ namespace PSFilterHostDll.PSApi
 		private bool disposed;
 
 		private static readonly Encoding Windows1252Encoding = Encoding.GetEncoding(1252);
-		
+
 		/// <summary>
 		/// Initializes a new instance of the <see cref="ImageMetaData"/> class.
 		/// </summary>
@@ -63,13 +63,12 @@ namespace PSFilterHostDll.PSApi
 		}
 
 		/// <summary>
-		/// Reads the JPEG APP1 section to extract EXIF or XMP meta data.
+		/// Reads the JPEG APP1 section to extract the EXIF meta data.
 		/// </summary>
 		/// <param name="jpegData">The JPEG image byte array.</param>
-		/// <param name="exif">if set to <c>true</c> extract the EXIF meta data; otherwise extract the XMP meta data.</param>
 		/// <returns> The extracted data or null.</returns>
 		/// <exception cref="System.ArgumentNullException"><paramref name="jpegData"/> is null.</exception>
-		private static unsafe byte[] ReadJpegAPP1(byte[] jpegData, bool exif)
+		private static unsafe byte[] ExtractEXIFMetaData(byte[] jpegData)
 		{
 			if (jpegData == null)
 			{
@@ -94,44 +93,22 @@ namespace PSFilterHostDll.PSApi
 					{
 						p += 2; // skip the header bytes
 
-						string sig;
+						string sig = new string((sbyte*)p + 2, 0, 6, Windows1252Encoding);
 
-						if (exif)
+						if (sig == "Exif\0\0")
 						{
-							sig = new string((sbyte*)p + 2, 0, 6, Windows1252Encoding);
+							int exifLength = sectionLength - 8; // subtract the signature and section length size to get the data length. 
+							bytes = new byte[exifLength];
 
-							if (sig == "Exif\0\0")
-							{
-								int exifLength = sectionLength - 8; // subtract the signature and section length size to get the data length. 
-								bytes = new byte[exifLength];
-
-								Marshal.Copy(new IntPtr(p + 8), bytes, 0, exifLength);
-							}
-
-							p += sectionLength;
-						}
-						else
-						{
-							sig = new string((sbyte*)p + 2, 0, 29, Windows1252Encoding);
-
-							if (sig == "http://ns.adobe.com/xap/1.0/\0")
-							{
-								// TODO: The XMP extension packets are not supported, so the XMP data must be less that 65502 bytes in size.
-								int xmpLength = sectionLength - 31;
-								bytes = new byte[xmpLength];
-
-								Marshal.Copy(new IntPtr(p + 31), bytes, 0, xmpLength);
-							}
-
-							p += sectionLength;
+							Marshal.Copy(new IntPtr(p + 8), bytes, 0, exifLength);
 						}
 
+						p += sectionLength;
 					}
 					else
 					{
 						p += sectionLength + 2;
 					}
-
 				}
 			}
 
@@ -191,72 +168,61 @@ namespace PSFilterHostDll.PSApi
 				return false;
 			}
 
-			if (MetaDataConverter.IsJPEGMetaData(metaData))
+			if (exif)
 			{
-				try
+				BitmapMetadata exifMetaData = MetaDataConverter.GetEXIFMetaData(metaData);
+
+				if (exifMetaData == null)
 				{
-					if (exif)
-					{
-						if (metaData.GetQuery("/app1/ifd/exif") == null)
-						{
-							this.extractedExif = true;
-							return false;
-						}
-					}
-					else
-					{
-						if (metaData.GetQuery("/xmp") == null)
-						{
-							this.extractedXMP = true;
-							return false;
-						}
-					}
+					this.extractedExif = true;
+
+					return false;
 				}
-				catch (IOException)
+				metaData = exifMetaData;
+			}
+			else
+			{
+				BitmapMetadata xmpMetaData = MetaDataConverter.GetXMPMetaData(metaData);
+
+				if (xmpMetaData == null)
 				{
-					return false; // WINCODEC_ERR_INVALIDQUERYREQUEST
+					this.extractedXMP = true;
+
+					return false;
+				}
+				metaData = xmpMetaData;
+			}
+#endif
+
+			if (exif)
+			{
+				using (MemoryStream ms = new MemoryStream())
+				{
+#if GDIPLUS
+					this.image.Save(ms, ImageFormat.Jpeg);
+#else
+					JpegBitmapEncoder enc = new JpegBitmapEncoder();
+					enc.Frames.Add(BitmapFrame.Create(this.image, null, metaData, null));
+					enc.Save(ms);
+#endif
+					this.exifBytes = ExtractEXIFMetaData(ms.GetBuffer());
+					this.extractedExif = true;
+					bytes = this.exifBytes;
 				}
 			}
 			else
 			{
-				BitmapMetadata converted = MetaDataConverter.ConvertMetaDataToJPEG(metaData, exif);
-
-				if (converted == null)
+				using (MemoryStream ms = new MemoryStream())
 				{
-					if (exif)
-					{
-						this.extractedExif = true;
-					}
-					else
-					{
-						this.extractedXMP = true;
-					}
-
-					return false;
-				}
-				metaData = converted;
-			}
-#endif
-
-			using (MemoryStream ms = new MemoryStream())
-			{
 #if GDIPLUS
-				this.image.Save(ms, ImageFormat.Jpeg);
+					this.image.Save(ms, ImageFormat.Tiff);
 #else
-				JpegBitmapEncoder enc = new JpegBitmapEncoder();
-				enc.Frames.Add(BitmapFrame.Create(this.image, null, metaData, null));
-				enc.Save(ms);
+					TiffBitmapEncoder enc = new TiffBitmapEncoder();
+					enc.Frames.Add(BitmapFrame.Create(this.image, null, metaData, null));
+					enc.Save(ms);
 #endif
-				if (exif)
-				{
-					this.exifBytes = ReadJpegAPP1(ms.GetBuffer(), true);
-					this.extractedExif = true;					
-					bytes = this.exifBytes;
-				}
-				else
-				{
-					this.xmpBytes = ReadJpegAPP1(ms.GetBuffer(), false);
-					this.extractedXMP = true;					
+					this.xmpBytes = TiffReader.ExtractXMP(ms);
+					this.extractedXMP = true;
 					bytes = this.xmpBytes;
 				}
 			}
@@ -289,6 +255,162 @@ namespace PSFilterHostDll.PSApi
 					this.exifBytes = null;
 					this.xmpBytes = null;
 				}
+			}
+		}
+
+		private static class TiffReader
+		{
+			enum DataType : ushort
+			{
+				Byte = 1,
+				Ascii = 2,
+				Short = 3,
+				Long = 4,
+				Rational = 5,
+				SByte = 6,
+				Undefined = 7,
+				SShort = 8,
+				SLong = 9,
+				SRational = 10,
+				Float = 11,
+				Double = 12
+			}
+
+			struct IFD
+			{
+				public ushort tag;
+				public DataType type;
+				public uint count;
+				public uint offset;
+
+				public IFD(Stream stream, bool littleEndian)
+				{
+					this.tag = ReadShort(stream, littleEndian);
+					this.type = (DataType)ReadShort(stream, littleEndian);
+					this.count = ReadLong(stream, littleEndian);
+					this.offset = ReadLong(stream, littleEndian);
+				}
+			}
+
+			private static ushort ReadShort(Stream stream, bool littleEndian)
+			{
+				int byte1 = stream.ReadByte();
+				if (byte1 == -1)
+				{
+					throw new EndOfStreamException();
+				}
+
+				int byte2 = stream.ReadByte();
+				if (byte2 == -1)
+				{
+					throw new EndOfStreamException();
+				}
+
+				if (littleEndian)
+				{
+					return (ushort)(byte1 | (byte2 << 8));
+				}
+				else
+				{
+					return (ushort)((byte1 << 8) | byte2);
+				}
+			}
+
+			private static uint ReadLong(Stream stream, bool littleEndian)
+			{
+				int byte1 = stream.ReadByte();
+				if (byte1 == -1)
+				{
+					throw new EndOfStreamException();
+				}
+
+				int byte2 = stream.ReadByte();
+				if (byte2 == -1)
+				{
+					throw new EndOfStreamException();
+				}
+
+				int byte3 = stream.ReadByte();
+				if (byte3 == -1)
+				{
+					throw new EndOfStreamException();
+				}
+
+				int byte4 = stream.ReadByte();
+				if (byte4 == -1)
+				{
+					throw new EndOfStreamException();
+				}
+
+				if (littleEndian)
+				{
+					return (uint)(byte1 | (((byte2 << 8) | (byte3 << 16)) | (byte4 << 24)));
+				}
+				else
+				{
+					return (uint)((((byte1 << 24) | (byte2 << 16)) | (byte3 << 8)) | byte4);
+				}
+			}
+
+			private const ushort LittleEndianByteOrder = 0x4949;
+			private const ushort TIFFSignature = 42;
+			private const ushort XmpTag = 700;
+
+			/// <summary>
+			/// Extracts the XMP packet from a TIFF file.
+			/// </summary>
+			/// <param name="stream">The stream to read.</param>
+			/// <returns>The extracted XMP packet, or null.</returns>
+			internal static byte[] ExtractXMP(Stream stream)
+			{
+				stream.Position = 0L;
+
+				try
+				{
+					ushort byteOrder = ReadShort(stream, false);
+
+					bool littleEndian = byteOrder == LittleEndianByteOrder;
+
+					ushort signature = ReadShort(stream, littleEndian);
+
+					if (signature == TIFFSignature)
+					{
+						uint ifdOffset = ReadLong(stream, littleEndian);
+						stream.Seek((long)ifdOffset, SeekOrigin.Begin);
+
+						int ifdCount = ReadShort(stream, littleEndian);
+
+						for (int i = 0; i < ifdCount; i++)
+						{
+							IFD ifd = new IFD(stream, littleEndian);
+
+							if (ifd.tag == XmpTag && (ifd.type == DataType.Byte || ifd.type == DataType.Undefined))
+							{
+								stream.Seek((long)ifd.offset, SeekOrigin.Begin);
+
+								int count = (int)ifd.count;
+
+								byte[] xmpBytes = new byte[count];
+
+								int numBytesToRead = count;
+								int numBytesRead = 0;
+								do
+								{
+									int n = stream.Read(xmpBytes, numBytesRead, numBytesToRead);
+									numBytesRead += n;
+									numBytesToRead -= n;
+								} while (numBytesToRead > 0);
+
+								return xmpBytes;
+							}
+						}
+					}
+				}
+				catch (EndOfStreamException)
+				{
+				}
+
+				return null;
 			}
 		}
 	}
