a47a8c379e8bc87e1c55af91cad0224f90108c62
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2015-08-29 21:16:49 -0600

    Removed SetLastError = false from the DLLImports as it is the default.

--- PSApi/SafeNativeMethods.cs
+++ PSApi/SafeNativeMethods.cs
@@ -21,43 +21,43 @@ namespace PSFilterHostDll.PSApi
         [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
         internal static extern IntPtr HeapCreate([In()] uint flOptions, [In()] UIntPtr dwInitialsize, [In()] UIntPtr dwMaximumSize);
 
-        [DllImport("kernel32.dll", SetLastError = false, ExactSpelling = true)]
+        [DllImport("kernel32.dll", ExactSpelling = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
         internal static extern bool HeapDestroy([In()] IntPtr hHeap);
 
         [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
         internal static extern IntPtr GetProcessHeap();
 
-        [DllImport("kernel32.dll", SetLastError = false, ExactSpelling = true)]
+        [DllImport("kernel32.dll", ExactSpelling = true)]
         internal static extern IntPtr HeapAlloc([In()] IntPtr hHeap, [In()] uint dwFlags, [In()] UIntPtr dwSize);
 
         [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
         internal static extern bool HeapFree([In()] IntPtr hHeap, [In()] uint dwFlags, [In()] IntPtr lpMem);
 
-        [DllImport("kernel32.dll", SetLastError = false, ExactSpelling = true)]
+        [DllImport("kernel32.dll", ExactSpelling = true)]
         internal static extern IntPtr HeapReAlloc([In()] IntPtr hHeap, [In()] uint dwFlags, [In()] IntPtr lpMem, [In()] UIntPtr dwBytes);
 
-        [DllImport("kernel32.dll", SetLastError = false, ExactSpelling = true)]
+        [DllImport("kernel32.dll", ExactSpelling = true)]
         internal static extern UIntPtr HeapSize([In()] IntPtr hHeap, [In()] uint dwFlags, [In()] IntPtr lpMem);
 
-        [DllImport("kernel32.dll", SetLastError = false, ExactSpelling = true)]
+        [DllImport("kernel32.dll", ExactSpelling = true)]
         internal static extern unsafe uint HeapSetInformation([In()] IntPtr HeapHandle, [In()] int HeapInformationClass, [In()] void* HeapInformation, [In()] UIntPtr HeapInformationLength);
 
-        [DllImport("kernel32.dll", SetLastError = false, ExactSpelling = true)]
+        [DllImport("kernel32.dll", ExactSpelling = true)]
         internal static extern IntPtr VirtualAlloc([In()] IntPtr lpAddress, [In()] UIntPtr dwSize, [In()] uint flAllocationType, [In()] uint flProtect);
 
         [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
         internal static extern bool VirtualFree([In()] IntPtr lpAddress, [In()] UIntPtr dwSize, [In()] uint dwFreeType);
 
-        [DllImport("kernel32.dll", SetLastError = false, ExactSpelling = true)]
+        [DllImport("kernel32.dll", ExactSpelling = true)]
         internal static extern unsafe UIntPtr VirtualQuery([In()] IntPtr address, ref NativeStructs.MEMORY_BASIC_INFORMATION buffer, [In()] UIntPtr sizeOfBuffer);
 
-        [DllImport("msvcrt.dll", CallingConvention = CallingConvention.Cdecl, SetLastError = false, ExactSpelling = true)]
+        [DllImport("msvcrt.dll", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
         internal static extern unsafe void memcpy([In()] void* dst, [In()] void* src, [In()] UIntPtr length);
 
-        [DllImport("msvcrt.dll", CallingConvention = CallingConvention.Cdecl, SetLastError = false, ExactSpelling = true)]
+        [DllImport("msvcrt.dll", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
         internal static extern IntPtr memset([In()] IntPtr dest, [In()] int c, [In()] UIntPtr count);
 
         [DllImport("user32.dll", CharSet = CharSet.Unicode, ExactSpelling = true)]
