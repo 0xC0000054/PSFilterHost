f4a22277d155220b74edf0f6b918b0bc8b4ac8e3
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2016-12-15 04:44:28 -0700

    Added ColorPickerManager.cs.

---
+++ PSApi/ColorPickerManager.cs
@@ -0,0 +1,63 @@
+﻿/////////////////////////////////////////////////////////////////////////////////
+//
+// 8bf filter host for .NET
+// http://psfilterhost.codeplex.com/
+//
+// This software is provided under the Microsoft Public License:
+//   Copyright (C) 2012-2016 Nicholas Hayes
+// 
+// See LICENSE.txt for complete licensing and attribution information.
+//
+/////////////////////////////////////////////////////////////////////////////////
+
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace PSFilterHostDll.PSApi
+{
+    internal static class ColorPickerManager
+    {
+        private static PickColor pickColor;
+
+        public static void SetPickColorCallback(PickColor value)
+        {
+            pickColor = value;
+        }
+
+        public static bool ShowColorPickerDialog(string prompt, ref byte red, ref byte green, ref byte blue)
+        {
+            bool colorPicked = false;
+
+            if (pickColor != null)
+            {
+                ColorPickerResult color = pickColor(prompt == null ? string.Empty : prompt, red, green, blue);
+
+                if (color != null)
+                {
+                    red = color.R;
+                    green = color.G;
+                    blue = color.B;
+                    colorPicked = true;
+                }
+            }
+            else
+            {
+                using (ColorPicker picker = new ColorPicker(prompt))
+                {
+                    picker.Color = Color.FromArgb(red, green, blue);
+
+                    if (picker.ShowDialog() == DialogResult.OK)
+                    {
+                        Color color = picker.Color;
+                        red = color.R;
+                        green = color.G;
+                        blue = color.B;
+                        colorPicked = true;
+                    }
+                }
+            }
+
+            return colorPicked;
+        }
+    }
+}
