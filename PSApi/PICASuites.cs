7ebf245671ad410f84eef40acf9d425b4d7b6f3c
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2016-12-15 04:42:51 -0700

    Added a colorSpaceSuite field and changed CreateColorSpaceSuite1 to an instance method.

--- PSApi/PICASuites.cs
+++ PSApi/PICASuites.cs
@@ -18,12 +18,18 @@ namespace PSFilterHostDll.PSApi
 	internal sealed class PICASuites : IDisposable
 	{
 		private PICABufferSuite bufferSuite;
+#if PICASUITEDEBUG
+		private PICAColorSpaceSuite colorSpaceSuite;
+#endif
 		private PICAUIHooksSuite uiHooksSuite;
 		private string pluginName;
 
 		public PICASuites()
 		{
 			this.bufferSuite = null;
+#if PICASUITEDEBUG
+			this.colorSpaceSuite = null;
+#endif
 			this.uiHooksSuite = null;
 			this.pluginName = string.Empty;
 		}
@@ -59,9 +65,14 @@ namespace PSFilterHostDll.PSApi
 		}
 
 #if PICASUITEDEBUG
-		public static PSColorSpaceSuite1 CreateColorSpaceSuite1()
+		public PSColorSpaceSuite1 CreateColorSpaceSuite1()
 		{
-			return PICAColorSpaceSuite.CreateColorSpaceSuite1();
+			if (colorSpaceSuite == null)
+			{
+				this.colorSpaceSuite = new PICAColorSpaceSuite();
+			}
+
+			return this.colorSpaceSuite.CreateColorSpaceSuite1();
 		} 
 #endif
 
