0xC0000054/PSFilterHost

commit 374d1d332cf1f180601aa0227c29a06c272fd277

Reduce memory usage when showing filter previews
Use a 24-bit surface when displaying preview images that do not have an
alpha channel.
Changed the planar RGB color correction to use a 24-bit surface.

PSApi/LoadPsFilter.cs
@@ -88,11 +88,10 @@ namespace PSFilterHostDll.PSApi
         private SurfaceBase source;
         private SurfaceBase dest;
         private SurfaceGray8 mask;
-        private SurfaceBGRA32 displaySurface;
+        private IDisplayPixelsSurface displaySurface;
         private SurfaceGray8 tempMask;
         private SurfaceBase tempSurface;
         private Bitmap checkerBoardBitmap;
-        private SurfaceBGRA32 colorCorrectedDisplaySurface;
 
         private PluginPhase previousPhase;
         private PluginModule module;
@@ -3268,9 +3267,12 @@ namespace PSFilterHostDll.PSApi
             return PSError.noErr;
         }
 
-        private void SetupDisplaySurface(int width, int height, bool haveMask, int displayImageMode)
+        private void SetupDisplaySurface(int width, int height, bool haveTransparencyMask, int displayImageMode)
         {
-            if ((displaySurface == null) || width != displaySurface.Width || height != displaySurface.Height)
+            if ((displaySurface == null) ||
+                width != displaySurface.Width ||
+                height != displaySurface.Height ||
+                haveTransparencyMask != displaySurface.SupportsTransparency)
             {
                 if (displaySurface != null)
                 {
@@ -3278,41 +3280,15 @@ namespace PSFilterHostDll.PSApi
                     displaySurface = null;
                 }
 
-                displaySurface = new SurfaceBGRA32(width, height);
 
-                if (!haveMask)
+                if (haveTransparencyMask)
                 {
-                    displaySurface.SetAlphaToOpaque();
+                    displaySurface = new SurfaceBGRA32(width, height);
                 }
-
-                // As some plug-ins may use planar order RGB data and the Windows Color System APIs do not support that format
-                // we first have to convert the data to interleaved BGR(A) and then use a second surface for color correction.
-                if (displayImageMode == PSConstants.plugInModeRGBColor && colorProfileConverter.ColorCorrectionRequired)
+                else
                 {
-                    if (colorCorrectedDisplaySurface != null)
-                    {
-                        colorCorrectedDisplaySurface.Dispose();
-                        colorCorrectedDisplaySurface = null;
-                    }
-
-                    colorCorrectedDisplaySurface = new SurfaceBGRA32(width, height);
+                    displaySurface = new SurfaceBGR24(width, height);
                 }
-            }
-        }
-
-        /// <summary>
-        /// Gets the preview bitmap for the RGB image modes and applies color correction if necessary.
-        /// </summary>
-        /// <returns>The resulting bitmap.</returns>
-        private Bitmap GetRGBPreviewBitmap()
-        {
-            if (colorProfileConverter.ColorCorrectionRequired && colorProfileConverter.ColorCorrectBGRASurface(displaySurface, colorCorrectedDisplaySurface))
-            {
-                return colorCorrectedDisplaySurface.CreateAliasedBitmap();
-            }
-            else
-            {
-                return displaySurface.CreateAliasedBitmap();
             }
         }
 
@@ -3329,7 +3305,7 @@ namespace PSFilterHostDll.PSApi
             // Skip the rendering of the checker board if the surface does not contain any transparency.
             if (allOpaque)
             {
-                using (Bitmap bmp = GetRGBPreviewBitmap())
+                using (Bitmap bmp = displaySurface.CreateAliasedBitmap())
                 {
                     gr.DrawImageUnscaled(bmp, dstCol, dstRow);
                 }
@@ -3354,7 +3330,7 @@ namespace PSFilterHostDll.PSApi
                         using (Graphics tempGr = Graphics.FromImage(temp))
                         {
                             tempGr.DrawImageUnscaledAndClipped(checkerBoardBitmap, rect);
-                            using (Bitmap bmp = GetRGBPreviewBitmap())
+                            using (Bitmap bmp = displaySurface.CreateAliasedBitmap())
                             {
                                 tempGr.DrawImageUnscaled(bmp, rect);
                             }
@@ -3435,48 +3411,58 @@ namespace PSFilterHostDll.PSApi
             }
             else
             {
-                if (srcPixelMap.colBytes == 1)
+                // Perform color correction if required and fall back to the uncorrected data if it fails.
+                if (!colorProfileConverter.ColorCorrectionRequired ||
+                    !colorProfileConverter.ColorCorrectRGB(srcPixelMap.baseAddr,
+                                                           srcPixelMap.rowBytes,
+                                                           srcPixelMap.colBytes,
+                                                           srcPixelMap.planeBytes,
+                                                           new Point(left, top),
+                                                           displaySurface))
                 {
-                    int greenPlaneOffset = srcPixelMap.planeBytes;
-                    int bluePlaneOffset = srcPixelMap.planeBytes * 2;
-                    for (int y = top; y < bottom; y++)
+                    if (srcPixelMap.colBytes == 1)
                     {
-                        byte* redPlane = baseAddr + (y * srcPixelMap.rowBytes) + left;
-                        byte* greenPlane = redPlane + greenPlaneOffset;
-                        byte* bluePlane = redPlane + bluePlaneOffset;
-
-                        byte* dst = displaySurface.GetRowAddressUnchecked(y - top);
-
-                        for (int x = 0; x < width; x++)
+                        int greenPlaneOffset = srcPixelMap.planeBytes;
+                        int bluePlaneOffset = srcPixelMap.planeBytes * 2;
+                        for (int y = top; y < bottom; y++)
                         {
-                            dst[2] = *redPlane;
-                            dst[1] = *greenPlane;
-                            dst[0] = *bluePlane;
+                            byte* redPlane = baseAddr + (y * srcPixelMap.rowBytes) + left;
+                            byte* greenPlane = redPlane + greenPlaneOffset;
+                            byte* bluePlane = redPlane + bluePlaneOffset;
 
-                            redPlane++;
-                            greenPlane++;
-                            bluePlane++;
-                            dst += 4;
+                            byte* dst = displaySurface.GetRowAddressUnchecked(y - top);
+
+                            for (int x = 0; x < width; x++)
+                            {
+                                dst[2] = *redPlane;
+                                dst[1] = *greenPlane;
+                                dst[0] = *bluePlane;
+
+                                redPlane++;
+                                greenPlane++;
+                                bluePlane++;
+                                dst += 4;
+                            }
                         }
                     }
-                }
-                else
-                {
-                    for (int y = top; y < bottom; y++)
+                    else
                     {
-                        byte* src = baseAddr + (y * srcPixelMap.rowBytes) + (left * srcPixelMap.colBytes);
-                        byte* dst = displaySurface.GetRowAddressUnchecked(y - top);
-
-                        for (int x = 0; x < width; x++)
+                        for (int y = top; y < bottom; y++)
                         {
-                            dst[0] = src[2];
-                            dst[1] = src[1];
-                            dst[2] = src[0];
+                            byte* src = baseAddr + (y * srcPixelMap.rowBytes) + (left * srcPixelMap.colBytes);
+                            byte* dst = displaySurface.GetRowAddressUnchecked(y - top);
 
-                            src += srcPixelMap.colBytes;
-                            dst += 4;
+                            for (int x = 0; x < width; x++)
+                            {
+                                dst[0] = src[2];
+                                dst[1] = src[1];
+                                dst[2] = src[0];
+
+                                src += srcPixelMap.colBytes;
+                                dst += 4;
+                            }
                         }
-                    }
+                    } 
                 }
             }
 
@@ -3517,7 +3503,7 @@ namespace PSFilterHostDll.PSApi
                     }
                     else
                     {
-                        using (Bitmap bmp = GetRGBPreviewBitmap())
+                        using (Bitmap bmp = displaySurface.CreateAliasedBitmap())
                         {
                             gr.DrawImageUnscaled(bmp, dstCol, dstRow);
                         }
@@ -4032,12 +4018,6 @@ namespace PSFilterHostDll.PSApi
                     {
                         propertySuite.Dispose();
                         propertySuite = null;
-                    }
-
-                    if (colorCorrectedDisplaySurface != null)
-                    {
-                        colorCorrectedDisplaySurface.Dispose();
-                        colorCorrectedDisplaySurface = null;
                     }
 
                     if (colorProfileConverter != null)

Imaging/IColorManagedSurface.cs
@@ -1,0 +1,21 @@
+﻿/////////////////////////////////////////////////////////////////////////////////
+//
+// 8bf filter host for .NET
+// https://github.com/0xC0000054/PSFilterHost
+//
+// This software is provided under the Microsoft Public License:
+//   Copyright (C) 2012-2019 Nicholas Hayes
+//
+// See LICENSE.txt for complete licensing and attribution information.
+//
+/////////////////////////////////////////////////////////////////////////////////
+
+using PSFilterHostDll.Interop;
+
+namespace PSFilterHostDll.Imaging
+{
+    internal interface IColorManagedSurface : ISurfaceBase
+    {
+        NativeEnums.Mscms.BMFORMAT MscmsFormat { get; }
+    }
+}

Imaging/IDisplayPixelsSurface.cs
@@ -1,0 +1,21 @@
+﻿/////////////////////////////////////////////////////////////////////////////////
+//
+// 8bf filter host for .NET
+// https://github.com/0xC0000054/PSFilterHost
+//
+// This software is provided under the Microsoft Public License:
+//   Copyright (C) 2012-2019 Nicholas Hayes
+//
+// See LICENSE.txt for complete licensing and attribution information.
+//
+/////////////////////////////////////////////////////////////////////////////////
+
+namespace PSFilterHostDll.Imaging
+{
+    internal interface IDisplayPixelsSurface : ISurfaceBase, IColorManagedSurface
+    {
+        System.Drawing.Bitmap CreateAliasedBitmap();
+
+        bool SupportsTransparency { get; }
+    }
+}

Imaging/ISurfaceBase.cs
@@ -1,0 +1,33 @@
+﻿/////////////////////////////////////////////////////////////////////////////////
+//
+// 8bf filter host for .NET
+// https://github.com/0xC0000054/PSFilterHost
+//
+// This software is provided under the Microsoft Public License:
+//   Copyright (C) 2012-2019 Nicholas Hayes
+//
+// See LICENSE.txt for complete licensing and attribution information.
+//
+/////////////////////////////////////////////////////////////////////////////////
+
+using System;
+
+namespace PSFilterHostDll.Imaging
+{
+    internal interface ISurfaceBase : IDisposable
+    {
+        int Width { get; }
+
+        int Height { get; }
+
+        MemoryBlock Scan0 { get; }
+
+        long Stride { get; }
+
+        int BitsPerChannel { get; }
+
+        int ChannelCount { get; }
+
+        unsafe byte* GetRowAddressUnchecked(int y);
+    }
+}

Imaging/Pixel Structures/ColorBgr8.cs
@@ -1,0 +1,29 @@
+﻿/////////////////////////////////////////////////////////////////////////////////
+//
+// 8bf filter host for .NET
+// https://github.com/0xC0000054/PSFilterHost
+//
+// This software is provided under the Microsoft Public License:
+//   Copyright (C) 2012-2019 Nicholas Hayes
+//
+// See LICENSE.txt for complete licensing and attribution information.
+//
+/////////////////////////////////////////////////////////////////////////////////
+
+using System.Runtime.InteropServices;
+
+namespace PSFilterHostDll.Imaging
+{
+    [StructLayout(LayoutKind.Explicit)]
+    internal struct ColorBgr8
+    {
+        [FieldOffset(0)]
+        public byte B;
+
+        [FieldOffset(1)]
+        public byte G;
+
+        [FieldOffset(2)]
+        public byte R;
+    }
+}

Imaging/SurfaceBGR24.cs
@@ -1,0 +1,227 @@
+﻿/////////////////////////////////////////////////////////////////////////////////
+//
+// 8bf filter host for .NET
+// https://github.com/0xC0000054/PSFilterHost
+//
+// This software is provided under the Microsoft Public License:
+//   Copyright (C) 2012-2019 Nicholas Hayes
+//
+// See LICENSE.txt for complete licensing and attribution information.
+//
+/////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Drawing;
+using PSFilterHostDll.Interop;
+
+namespace PSFilterHostDll.Imaging
+{
+    /// <summary>
+    /// Surface class for 24 bits per pixel BGR image data. Each channel is allocated 8 bits per pixel.
+    /// </summary>
+    internal sealed class SurfaceBGR24 : SurfaceBase, IColorManagedSurface, IDisplayPixelsSurface
+    {
+        public SurfaceBGR24(int width, int height) : this(width, height, 96.0, 96.0)
+        {
+        }
+
+        public SurfaceBGR24(int width, int height, double dpiX, double dpiY) : base(width, height, 3, dpiX, dpiY, SurfaceCreationOptions.GdiPlusCompatableStride)
+        {
+        }
+
+        public override int ChannelCount => 3;
+
+        public override int BitsPerChannel => 8;
+
+        public NativeEnums.Mscms.BMFORMAT MscmsFormat => NativeEnums.Mscms.BMFORMAT.BM_RGBTRIPLETS;
+
+        Bitmap IDisplayPixelsSurface.CreateAliasedBitmap()
+        {
+            return new Bitmap(width, height, (int)stride, System.Drawing.Imaging.PixelFormat.Format24bppRgb, scan0.Pointer);
+        }
+
+        bool IDisplayPixelsSurface.SupportsTransparency => false;
+
+#if GDIPLUS
+        public override unsafe Bitmap ToGdipBitmap()
+        {
+            Bitmap image = null;
+
+            const System.Drawing.Imaging.PixelFormat format = System.Drawing.Imaging.PixelFormat.Format24bppRgb;
+
+            using (Bitmap temp = new Bitmap(width, height, format))
+            {
+                System.Drawing.Imaging.BitmapData bitmapData = temp.LockBits(new Rectangle(0, 0, width, height), System.Drawing.Imaging.ImageLockMode.WriteOnly, format);
+                try
+                {
+                    byte* destScan0 = (byte*)bitmapData.Scan0;
+                    int destStride = bitmapData.Stride;
+
+                    for (int y = 0; y < height; y++)
+                    {
+                        ColorBgr8* src = (ColorBgr8*)GetRowAddressUnchecked(y);
+                        byte* dst = destScan0 + (y * destStride);
+
+                        for (int x = 0; x < width; x++)
+                        {
+                            dst[0] = src->B;
+                            dst[1] = src->G;
+                            dst[2] = src->R;
+
+                            src++;
+                            dst += 3;
+                        }
+                    }
+                }
+                finally
+                {
+                    temp.UnlockBits(bitmapData);
+                }
+
+                image = (Bitmap)temp.Clone();
+            }
+
+            return image;
+        }
+#else
+        public override unsafe System.Windows.Media.Imaging.BitmapSource ToBitmapSource()
+        {
+            System.Windows.Media.Imaging.WriteableBitmap bitmap = new System.Windows.Media.Imaging.WriteableBitmap(
+                    width,
+                    height,
+                    dpiX,
+                    dpiY,
+                    System.Windows.Media.PixelFormats.Bgr24,
+                    null);
+
+            bitmap.Lock();
+            try
+            {
+                byte* destScan0 = (byte*)bitmap.BackBuffer;
+                int destStride = bitmap.BackBufferStride;
+
+                for (int y = 0; y < height; y++)
+                {
+                    ColorBgra8* src = (ColorBgra8*)GetRowAddressUnchecked(y);
+                    byte* dst = destScan0 + (y * destStride);
+
+                    for (int x = 0; x < width; x++)
+                    {
+                        dst[0] = src->B;
+                        dst[1] = src->G;
+                        dst[2] = src->R;
+
+                        src++;
+                        dst += 3;
+                    }
+                }
+            }
+            finally
+            {
+                bitmap.Unlock();
+            }
+
+            bitmap.Freeze();
+
+            return bitmap;
+        }
+#endif
+
+        protected override unsafe void FitSurfaceImpl(SurfaceBase source)
+        {
+            float lastRowIndex = height - 1;
+            float lastColumnIndex = width - 1;
+
+            IntPtr srcColCachePtr = IntPtr.Zero;
+
+            try
+            {
+                srcColCachePtr = ImageSurfaceMemory.Allocate((ulong)width * sizeof(float));
+                float* srcColCache = (float*)srcColCachePtr;
+
+                // Precompute the source column indexes.
+                for (int x = 0; x < width; x++)
+                {
+                    float u = x / lastColumnIndex;
+
+                    srcColCache[x] = (u * source.Width) - 0.5f;
+                }
+
+                for (int y = 0; y < height; y++)
+                {
+                    ColorBgr8* destRow = (ColorBgr8*)GetRowAddressUnchecked(y);
+                    float v = y / lastRowIndex;
+
+                    float srcY = (v * source.Height) - 0.5f;
+                    int yint = (int)srcY;
+                    float yfract = srcY - (float)Math.Floor(srcY);
+
+                    for (int x = 0; x < width; x++)
+                    {
+                        float srcX = srcColCache[x];
+                        int xint = (int)srcX;
+                        float xfract = srcX - (float)Math.Floor(srcX);
+
+                        // 1st row
+                        ColorBgr8 p00 = *(ColorBgr8*)source.GetPointAddressClamped(xint - 1, yint - 1);
+                        ColorBgr8 p10 = *(ColorBgr8*)source.GetPointAddressClamped(xint + 0, yint - 1);
+                        ColorBgr8 p20 = *(ColorBgr8*)source.GetPointAddressClamped(xint + 1, yint - 1);
+                        ColorBgr8 p30 = *(ColorBgr8*)source.GetPointAddressClamped(xint + 2, yint - 1);
+
+                        // 2nd row
+                        ColorBgr8 p01 = *(ColorBgr8*)source.GetPointAddressClamped(xint - 1, yint + 0);
+                        ColorBgr8 p11 = *(ColorBgr8*)source.GetPointAddressClamped(xint + 0, yint + 0);
+                        ColorBgr8 p21 = *(ColorBgr8*)source.GetPointAddressClamped(xint + 1, yint + 0);
+                        ColorBgr8 p31 = *(ColorBgr8*)source.GetPointAddressClamped(xint + 2, yint + 0);
+
+                        // 3rd row
+                        ColorBgr8 p02 = *(ColorBgr8*)source.GetPointAddressClamped(xint - 1, yint + 1);
+                        ColorBgr8 p12 = *(ColorBgr8*)source.GetPointAddressClamped(xint + 0, yint + 1);
+                        ColorBgr8 p22 = *(ColorBgr8*)source.GetPointAddressClamped(xint + 1, yint + 1);
+                        ColorBgr8 p32 = *(ColorBgr8*)source.GetPointAddressClamped(xint + 2, yint + 1);
+
+                        // 4th row
+                        ColorBgr8 p03 = *(ColorBgr8*)source.GetPointAddressClamped(xint - 1, yint + 2);
+                        ColorBgr8 p13 = *(ColorBgr8*)source.GetPointAddressClamped(xint + 0, yint + 2);
+                        ColorBgr8 p23 = *(ColorBgr8*)source.GetPointAddressClamped(xint + 1, yint + 2);
+                        ColorBgr8 p33 = *(ColorBgr8*)source.GetPointAddressClamped(xint + 2, yint + 2);
+
+                        float blue0 = BicubicUtil.CubicHermite(p00.B, p10.B, p20.B, p30.B, xfract);
+                        float blue1 = BicubicUtil.CubicHermite(p01.B, p11.B, p21.B, p31.B, xfract);
+                        float blue2 = BicubicUtil.CubicHermite(p02.B, p12.B, p22.B, p32.B, xfract);
+                        float blue3 = BicubicUtil.CubicHermite(p03.B, p13.B, p23.B, p33.B, xfract);
+
+                        float blue = BicubicUtil.CubicHermite(blue0, blue1, blue2, blue3, yfract);
+
+                        float green0 = BicubicUtil.CubicHermite(p00.G, p10.G, p20.G, p30.G, xfract);
+                        float green1 = BicubicUtil.CubicHermite(p01.G, p11.G, p21.G, p31.G, xfract);
+                        float green2 = BicubicUtil.CubicHermite(p02.G, p12.G, p22.G, p32.G, xfract);
+                        float green3 = BicubicUtil.CubicHermite(p03.G, p13.G, p23.G, p33.G, xfract);
+
+                        float green = BicubicUtil.CubicHermite(green0, green1, green2, green3, yfract);
+
+                        float red0 = BicubicUtil.CubicHermite(p00.R, p10.R, p20.R, p30.R, xfract);
+                        float red1 = BicubicUtil.CubicHermite(p01.R, p11.R, p21.R, p31.R, xfract);
+                        float red2 = BicubicUtil.CubicHermite(p02.R, p12.R, p22.R, p32.R, xfract);
+                        float red3 = BicubicUtil.CubicHermite(p03.R, p13.R, p23.R, p33.R, xfract);
+
+                        float red = BicubicUtil.CubicHermite(red0, red1, red2, red3, yfract);
+
+                        destRow->B = (byte)FloatUtil.Clamp(blue, 0, 255);
+                        destRow->G = (byte)FloatUtil.Clamp(green, 0, 255);
+                        destRow->R = (byte)FloatUtil.Clamp(red, 0, 255);
+                        destRow++;
+                    }
+                }
+            }
+            finally
+            {
+                if (srcColCachePtr != IntPtr.Zero)
+                {
+                    ImageSurfaceMemory.Free(srcColCachePtr);
+                    srcColCachePtr = IntPtr.Zero;
+                }
+            }
+        }
+    }
+}

Imaging/SurfaceBGRA32.cs
@@ -20,13 +20,14 @@
 /////////////////////////////////////////////////////////////////////////////////
 using System;
 using System.Drawing;
+using PSFilterHostDll.Interop;
 
 namespace PSFilterHostDll.Imaging
 {
     /// <summary>
     /// Surface class for 32 bits per pixel BGRA image data. Each channel is allocated 8 bits per pixel.
     /// </summary>
-    internal sealed class SurfaceBGRA32 : SurfaceBGRABase
+    internal sealed class SurfaceBGRA32 : SurfaceBGRABase, IColorManagedSurface, IDisplayPixelsSurface
     {
         public SurfaceBGRA32(int width, int height) : this(width, height, 96.0, 96.0)
         {
@@ -40,10 +41,14 @@ namespace PSFilterHostDll.Imaging
 
         public override int BitsPerChannel => 8;
 
-        public unsafe Bitmap CreateAliasedBitmap()
+        public NativeEnums.Mscms.BMFORMAT MscmsFormat => NativeEnums.Mscms.BMFORMAT.BM_xRGBQUADS;
+
+        Bitmap IDisplayPixelsSurface.CreateAliasedBitmap()
         {
             return new Bitmap(width, height, (int)stride, System.Drawing.Imaging.PixelFormat.Format32bppArgb, scan0.Pointer);
         }
+
+        bool IDisplayPixelsSurface.SupportsTransparency => true;
 
 #if GDIPLUS
         [System.Diagnostics.CodeAnalysis.SuppressMessage(

Imaging/SurfaceBase.cs
@@ -24,7 +24,7 @@ using System.Drawing;
 
 namespace PSFilterHostDll.Imaging
 {
-    internal abstract class SurfaceBase : IDisposable
+    internal abstract class SurfaceBase : IDisposable, ISurfaceBase
     {
         protected readonly int width;
         protected readonly int height;
@@ -67,12 +67,32 @@ namespace PSFilterHostDll.Imaging
         /// <param name="bytesPerPixel">The bytes per pixel.</param>
         /// <param name="dpiX">The horizontal resolution of the surface.</param>
         /// <param name="dpiY">The vertical resolution of the surface.</param>
-        public SurfaceBase(int width, int height, int bytesPerPixel, double dpiX, double dpiY)
+        protected SurfaceBase(int width, int height, int bytesPerPixel, double dpiX, double dpiY)
+            : this(width, height, bytesPerPixel, dpiX, dpiY, SurfaceCreationOptions.Default)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SurfaceBase" /> class.
+        /// </summary>
+        /// <param name="width">The width.</param>
+        /// <param name="height">The height.</param>
+        /// <param name="bytesPerPixel">The bytes per pixel.</param>
+        /// <param name="dpiX">The horizontal resolution of the surface.</param>
+        /// <param name="dpiY">The vertical resolution of the surface.</param>
+        /// <param name="options">The surface creation options.</param>
+        protected SurfaceBase(int width, int height, int bytesPerPixel, double dpiX, double dpiY, SurfaceCreationOptions options)
         {
             this.width = width;
             this.height = height;
             this.bytesPerPixel = bytesPerPixel;
             stride = width * bytesPerPixel;
+            if ((options & SurfaceCreationOptions.GdiPlusCompatableStride) == SurfaceCreationOptions.GdiPlusCompatableStride)
+            {
+                // GDI+ expects the stride to be a multiple of four.
+                stride = (stride + 3) & ~3;
+            }
+
             this.dpiX = dpiX;
             this.dpiY = dpiY;
             scan0 = new MemoryBlock(stride * height);

commit 6bc0b4aed2e59a2d4034981c16c379ec647fb828

Fix an out-of-bounds write in DisplayPixelsProc
Use the value of the displaySurface ChannelCount property when stepping
through the columns.

This fixes a regression introduced in 374d1d332cf1f180601aa0227c29a06c272fd277

PSApi/LoadPsFilter.cs
@@ -3311,6 +3311,8 @@ namespace PSFilterHostDll.PSApi
                 bottom = height;
             }
 
+            int destColBytes = displaySurface.ChannelCount;
+
             if (srcPixelMap.imageMode == PSConstants.plugInModeGrayScale)
             {
                 // Perform color correction if required and fall back to the uncorrected data if it fails.
@@ -3327,7 +3329,7 @@ namespace PSFilterHostDll.PSApi
                             dst[0] = dst[1] = dst[2] = *src;
 
                             src += srcPixelMap.colBytes;
-                            dst += 4;
+                            dst += destColBytes;
                         }
                     }
                 }
@@ -3364,7 +3366,7 @@ namespace PSFilterHostDll.PSApi
                                 redPlane++;
                                 greenPlane++;
                                 bluePlane++;
-                                dst += 4;
+                                dst += destColBytes;
                             }
                         }
                     }
@@ -3382,7 +3384,7 @@ namespace PSFilterHostDll.PSApi
                                 dst[2] = src[0];
 
                                 src += srcPixelMap.colBytes;
-                                dst += 4;
+                                dst += destColBytes;
                             }
                         }
                     }
@@ -3417,7 +3419,7 @@ namespace PSFilterHostDll.PSApi
                                     }
 
                                     src += srcMask->colBytes;
-                                    dst += 4;
+                                    dst += destColBytes;
                                 }
                             }
                         }